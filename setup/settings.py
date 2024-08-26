d6b339502210e38130d2d4f4b47f1e6696a7ff5f
César L. Guilherme <clguilherme3@gmail.com>
2024-08-26 17:44:32 -0300

    Autenticação de formulário e configuração dos alertas

--- setup/settings.py
+++ setup/settings.py
@@ -140,3 +140,11 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
--- usuarios/forms.py
+++ usuarios/forms.py
@@ -69,3 +69,23 @@ class CadastroForms(forms.Form):
             }
         )
     )
+
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get("nome_cadastro")
+
+        if nome:
+            nome = nome.strip()
+            if " " in nome:
+                raise forms.ValidationError("Espaços não são permitidos para este campo!")
+            else:
+                return nome
+            
+    def clean_senha_2(self):
+        senha_1 = self.cleaned_data.get("senha_1")
+        senha_2 = self.cleaned_data.get("senha_2")
+
+        if senha_1 and senha_2:
+            if senha_1 != senha_2:
+                raise forms.ValidationError("Senhas não são iguais!")
+            else:
+                return senha_2
