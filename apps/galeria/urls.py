e562803aff213d08b8b13e0b32d57d34b144898a
César L. Guilherme <clguilherme3@gmail.com>
2024-09-06 18:56:50 -0300

    API Artsy

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.galeria.views import index, imagem, buscar, nova_obra, editar_obra, deletar_obra, filtro
+from apps.galeria.views import index, imagem, buscar, nova_obra, editar_obra, deletar_obra, filtro, artwork_view, artist_search_view
 
 urlpatterns = [
     path('', index, name='index'),
@@ -9,4 +9,6 @@ urlpatterns = [
     path('editar-obra/<int:foto_id>', editar_obra, name="editar_obra"),
     path('deletar-obra/<int:foto_id>', deletar_obra, name="deletar_obra"),
     path('filtro/<str:categoria>', filtro, name='filtro'),
+    path('artwork/<str:artwork_id>/', artwork_view, name='artwork_view'),
+    path('search_artists/', artist_search_view, name='artist_search_view'),
 ]
--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
 from django.contrib import messages
 from apps.galeria.forms import FotografiaForms
+import requests
+from django.http import JsonResponse
 
 
 def index(request):
@@ -16,17 +18,40 @@ def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia":fotografia})
 
+# Usar o token de acesso para fazer uma solicitação
+def search_artists(token, query):
+    url = f'https://api.artsy.net/api/search?q={query}&type=artist'
+    headers = {
+        'X-Xapp-Token': token
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
 def buscar(request):
     if not request.user.is_authenticated:
-        messages.error(request,"Usuário não logado!")
-        return redirect ('login')
+        messages.error(request, "Usuário não logado!")
+        return redirect('login')
     
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+    artists = []
+    
     if "buscar" in request.GET:
         nome_a_buscar = request.GET['buscar']
         if nome_a_buscar:
+            # Filtra as fotografias pelo nome
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
-    return render(request, "galeria/index.html", {"cards":fotografias})
+            
+            # Busca artistas na API do Artsy
+            token = get_access_token()
+            artists_data = search_artists(token, nome_a_buscar)
+            for artist in artists_data.get('_embedded', {}).get('results', []):
+                artists.append({
+                    'title': artist.get('title'),
+                    'permalink': artist.get('_links', {}).get('permalink', {}).get('href'),
+                    'thumbnail': artist.get('_links', {}).get('thumbnail', {}).get('href')
+                })
+    
+    return render(request, "galeria/index.html", {"cards": fotografias, "artists": artists})
 
 def nova_obra(request):
     if not request.user.is_authenticated:
@@ -63,3 +88,59 @@ def deletar_obra(request, foto_id):
 def filtro(request, categoria):
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True, categoria = categoria)
     return render(request, 'galeria/index.html', {"cards": fotografias})
+
+# Suas credenciais
+client_id = '26cb47642d27045c9f4c'
+client_secret = 'bf7c7faa1426d74dbbf5b134bebea3ff'
+
+# Obter o token de acesso
+def get_access_token():
+    url = 'https://api.artsy.net/api/tokens/xapp_token'
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
+    response = requests.post(url, data=data)
+    token = response.json()['token']
+    return token
+
+# Usar o token de acesso para fazer uma solicitação
+def get_artwork(token, artwork_id):
+    url = f'https://api.artsy.net/api/artworks/{artwork_id}'
+    headers = {
+        'X-Xapp-Token': token
+    }
+    response = requests.get(url, headers=headers)
+    print(headers)  # Verifique os cabeçalhos
+    print(response.json())  # Verifique a resposta
+    return response.json()
+
+
+# Visão para exibir a obra de arte
+def artwork_view(request, artwork_id):
+    token = get_access_token()
+    artwork = get_artwork(token, artwork_id)
+    return JsonResponse(artwork)
+
+# Usar o token de acesso para fazer uma solicitação
+def search_artists(token, query):
+    url = f'https://api.artsy.net/api/search?q={query}&type=artist'
+    headers = {
+        'X-Xapp-Token': token
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+# Visão para buscar artistas e renderizar o template
+def artist_search_view(request):
+    query = request.GET.get('query', '')
+    token = get_access_token()
+    artists_data = search_artists(token, query)
+    artists = []
+    for artist in artists_data.get('_embedded', {}).get('results', []):
+        artists.append({
+            'title': artist.get('title'),
+            'permalink': artist.get('_links', {}).get('permalink', {}).get('href'),
+            'thumbnail': artist.get('_links', {}).get('thumbnail', {}).get('href')
+        })
+    return render(request, 'galeria/artist.html', {'artists': artists})
